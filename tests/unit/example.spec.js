350b45476531d42706016cb90316c7076d028c40
Tomasz Dworakowski <tomaszdworakowski@gmail.com>
2019-12-28 15:51:46 +0100

    Create an empty Vue/Vuetify app with example unit and e2e tests

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+Vue.use(Vuetify);
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "Welcome to Vuetify";
+    const wrapper = shallowMount(HelloWorld, {});
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
