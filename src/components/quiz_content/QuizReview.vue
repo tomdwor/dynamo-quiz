e7514210c06b34ca5041080d1025c472411aeffb
Tomasz Dworakowski <tomaszdworakowski@gmail.com>
2020-02-09 17:32:18 +0100

    Quiz review template - update

--- src/components/quiz_content/QuizReview.vue
+++ src/components/quiz_content/QuizReview.vue
@@ -4,24 +4,14 @@
       <v-col>
         <h2>Quiz review</h2>
         <p>Quantum mechanics</p>
-        <v-card
-          v-if="true"
-          outlined
-          color="green lighten-4"
-          class="mt-5"
-        >
+        <v-card v-if="true" outlined color="green lighten-4" class="mt-5">
           <v-list-item three-line>
             <v-list-item-content>
               PASS - 77%
             </v-list-item-content>
           </v-list-item>
         </v-card>
-        <v-card
-          v-else
-          outlined
-          color="red lighten-4"
-          class="mt-5"
-        >
+        <v-card v-else outlined color="red lighten-4" class="mt-5">
           <v-list-item three-line>
             <v-list-item-content>
               FAIL - 55%
@@ -30,9 +20,9 @@
         </v-card>
         <h3 class="mt-6">Stats:</h3>
         <p class="mt-2">
-          All questions: <strong>13</strong><br>
-          Correct: <strong>10</strong><br>
-          Incorrect: <strong>3</strong><br>
+          All questions: <strong>13</strong><br />
+          Correct: <strong>10</strong><br />
+          Incorrect: <strong>3</strong><br />
         </p>
       </v-col>
     </v-row>
