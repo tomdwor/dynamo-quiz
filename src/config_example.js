173212977e3cd7896de4d49d78548d39fbd9ba0d
Tomasz Dworakowski <tomaszdworakowski@gmail.com>
2021-02-25 18:56:40 +0100

    Adding example files for config and data

---
+++ src/config_example.js
@@ -0,0 +1,15 @@
+const QUIZZES_LIST_DATA_URL = "/data/quizzes-list.json";
+const QUIZ_DATA_URL_PREFIX = "/data/quiz/";
+const QUIZ_PAGE_NAME = "Dynamo Quiz";
+const QUIZ_PAGE_FOOTER = "Powered by Dynamo Quiz";
+const ABOUT_PAGE_CONTENT =
+  "DynamoQuiz is an open source quiz solution for web browsers.<br>" +
+  '<a href="https://github.com/tomdwor/dynamoquiz">github.com/tomdwor/dynamoquiz</a>.';
+
+export {
+  QUIZZES_LIST_DATA_URL,
+  QUIZ_DATA_URL_PREFIX,
+  QUIZ_PAGE_NAME,
+  QUIZ_PAGE_FOOTER,
+  ABOUT_PAGE_CONTENT
+};
